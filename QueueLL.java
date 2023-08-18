350727e7330611bccfb889d58045eede87b78b48
Khushboo Jaiswal <126168471+khushboo-9876@users.noreply.github.com>
2023-08-18 09:09:00 -0700

    Add files via upload

---
+++ QueueLL.java
@@ -0,0 +1,70 @@
+public class QueueLL{
+    static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            next=null;
+        }
+    }
+    static class Queue{
+        static Node head=null;
+        static Node tail=null;
+
+        public  boolean isEmpty(){
+            return head==null && tail==null;
+        }
+
+        public  void add(int data){
+            Node newNode = new Node(data);
+            if(isEmpty()){
+                tail=head=newNode;
+            }
+            else{
+                tail.next=newNode;
+                tail=newNode;
+            }
+        }
+
+        public  int remove(){
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            int front=head.data;
+            if(head==tail){
+                tail=null;
+            }
+            head=head.next;
+            return front;
+        }
+
+        public  int peek(){
+            if(isEmpty()){
+                System.out.println("empty queue");
+                return -1;
+            }
+
+            return head.data;
+        }
+    }
+    public static void main(String[] args) {
+        Queue q=new Queue();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+        q.remove();
+        System.out.println(q.remove());
+        q.add(6);
+        System.out.println(q.remove());
+        q.add(7);
+
+        while(!q.isEmpty()){
+            System.out.println(q.peek());
+            q.remove();
+        }
+    }
+}
